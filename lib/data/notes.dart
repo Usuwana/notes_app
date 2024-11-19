983859dad9c77f08e9b7403d57a52695beb15c0c
Tatenda Mapuranga <tatemapu@gmail.com>
2024-11-19 13:37:23 +1100

    .

--- lib/data/notes.dart
+++ lib/data/notes.dart
@@ -13,24 +13,28 @@ class ClientNotes {
   List<dynamic> titles = [];
   List<dynamic> notes = [];
   List<dynamic> updates_times = [];
+
+  List<dynamic> searchTitles = [];
+  List<dynamic> searchNotes = [];
+  List<dynamic> searchTimes = [];
   final firestoreInstance = FirebaseFirestore.instance;
 
-  void removeClient(String title) {
+  void removeNote(String title) {
     FirebaseAuth.instance.authStateChanges().listen((User? user) {
       if (user != null) {
         //print(user.uid);
         firestoreInstance
             .collection("allow-users")
             .doc(user.uid)
-            .collection("clients")
-            .where("name", isEqualTo: title)
+            .collection("notes")
+            .where("title", isEqualTo: title)
             .get()
             .then((value) {
           value.docs.forEach((element) {
             FirebaseFirestore.instance
                 .collection("allow-users")
                 .doc(user.uid)
-                .collection("clients")
+                .collection("notes")
                 .doc(element.id)
                 .delete()
                 .then((value) {
@@ -42,25 +46,20 @@ class ClientNotes {
     });
   }
 
-  Future<void> addClient(String name, String email, String id, String phone,
-      String details, String address, String diagnosis
-      /*List<dynamic> schedules*/
-      ) async {
+  Future<void> addNote(String title, String note, String creation_time,
+      String update_time) async {
     FirebaseAuth.instance.authStateChanges().listen((User? user) {
       if (user != null) {
         //print(user.uid);
         firestoreInstance
             .collection("allow-users")
             .doc(user.uid)
-            .collection("clients")
+            .collection("notes")
             .add({
-          'details': details,
-          'email': email,
-          'name': name,
-          'phone': phone,
-          'id': id,
-          'address': address,
-          'diagnosis': diagnosis
+          'title': title,
+          'note': note,
+          'creation_time': creation_time,
+          'update_time': update_time
         }).then((value) {
           print(value.id);
         });
@@ -68,7 +67,7 @@ class ClientNotes {
     });
   }
 
-  Future<void> editClientName(String current, String future) async {
+  Future<void> editnoteTitle(String current, String future) async {
     FirebaseAuth.instance.authStateChanges().listen((User? user) {
       if (user != null) {
         //print(user.uid);
@@ -98,7 +97,7 @@ class ClientNotes {
     //editClient('Tatenda');
   }
 
-  Future<void> editClientDetails(String current, String future) async {
+  Future<void> editNoteContent(String current, String future) async {
     FirebaseAuth.instance.authStateChanges().listen((User? user) {
       if (user != null) {
         //print(user.uid);
@@ -128,7 +127,7 @@ class ClientNotes {
     //editClient('Tatenda');
   }
 
-  Future<void> editClientDiagnosis(String current, String future) async {
+  Future<void> editUpdateTime(String current, String future) async {
     FirebaseAuth.instance.authStateChanges().listen((User? user) {
       if (user != null) {
         //print(user.uid);
@@ -147,36 +146,6 @@ class ClientNotes {
                 .collection("clients")
                 .doc(element.id)
                 .update({"diagnosis": future})
-                //.delete()
-                .then((value) {
-              print("Success!");
-            });
-          });
-        });
-      }
-    });
-    //editClient('Tatenda');
-  }
-
-  Future<void> editClientEmail(String current, String future) async {
-    FirebaseAuth.instance.authStateChanges().listen((User? user) {
-      if (user != null) {
-        //print(user.uid);
-        firestoreInstance
-            .collection("allow-users")
-            .doc(user.uid)
-            .collection("clients")
-            .where("email", isEqualTo: current)
-            .get()
-            .then((value) {
-          print("Documents found: ${value.docs.length}");
-          value.docs.forEach((element) {
-            FirebaseFirestore.instance
-                .collection("allow-users")
-                .doc(user.uid)
-                .collection("clients")
-                .doc(element.id)
-                .update({"email": future})
                 //.delete()
                 .then((value) {
               print("Success!");
@@ -227,38 +196,7 @@ class ClientNotes {
     }
   }
 
-  Future<dynamic> searchClient(String client) async {
-    /*await FirebaseAuth.instance.authStateChanges().listen((User? user) async {
-      if (user != null) {
-        //print(user.uid);
-        firestoreInstance
-            .collection("allow-users")
-            .doc(user.uid)
-            .collection("clients")
-            .where("name", isEqualTo: client)
-            .get()
-            .then((value) {
-          print("Documents found: ${value.docs.length}");
-          value.docs.forEach((element) {
-            searchNames.add(value.docs.map((doc) => doc["name"]).toList());
-            print('As we go:');
-            print(value.docs.map((doc) => doc["name"]).toList());
-            searchIDs.add(value.docs.map((doc) => doc["id"]).toList());
-            searchEmails.add(value.docs.map((doc) => doc["email"]).toList());
-            searchPhones.add(value.docs.map((doc) => doc["phone"]).toList());
-            searchDetails.add(value.docs.map((doc) => doc["details"]).toList());
-            searchAddresses
-                .add(value.docs.map((doc) => doc["address"]).toList());
-            searchDiagnoses
-                .add(value.docs.map((doc) => doc["diagnosis"]).toList());
-          });
-        });
-      }
-    });
-    print('At the end');
-    print(searchNames);
-    return searchNames;*/
-
+  Future<dynamic> searchNote(String note) async {
     // Wait for the auth state to return a user.
     User? user = await FirebaseAuth.instance
         .authStateChanges()
@@ -269,149 +207,39 @@ class ClientNotes {
       final querySnapshot = await firestoreInstance
           .collection("allow-users")
           .doc(user.uid)
-          .collection("clients")
+          .collection("notes")
           //.where("name", isEqualTo: client)
-          .where("name", isGreaterThanOrEqualTo: client)
-          .where("name", isLessThanOrEqualTo: client + '\uf8ff')
+          .where("title", isGreaterThanOrEqualTo: note)
+          .where("title", isLessThanOrEqualTo: note + '\uf8ff')
+          .where("note", isGreaterThanOrEqualTo: note)
+          .where("note", isLessThanOrEqualTo: note + '\uf8ff')
           .get();
 
       print("Documents found: ${querySnapshot.docs.length}");
-      searchNames = querySnapshot.docs.map((doc) => doc["name"]).toList();
+      searchTitles = querySnapshot.docs.map((doc) => doc["title"]).toList();
 
       print('As we go:');
-      print(searchNames);
+      print(searchTitles);
 
-      searchIDs.addAll(querySnapshot.docs.map((doc) => doc["id"]).toList());
-      searchEmails
-          .addAll(querySnapshot.docs.map((doc) => doc["email"]).toList());
-      searchPhones
-          .addAll(querySnapshot.docs.map((doc) => doc["phone"]).toList());
-      searchDetails
-          .addAll(querySnapshot.docs.map((doc) => doc["details"]).toList());
-      searchAddresses
-          .addAll(querySnapshot.docs.map((doc) => doc["address"]).toList());
-      searchDiagnoses
-          .addAll(querySnapshot.docs.map((doc) => doc["diagnosis"]).toList());
+      searchNotes.addAll(querySnapshot.docs.map((doc) => doc["note"]).toList());
     }
 
     print('At the end');
-    print(searchNames);
-    return searchNames;
+    print(searchTitles);
+    return searchTitles;
   }
 
-  Future<void> editClientAddress(String current, String future) async {
-    FirebaseAuth.instance.authStateChanges().listen((User? user) {
-      if (user != null) {
-        //print(user.uid);
-        firestoreInstance
-            .collection("allow-users")
-            .doc(user.uid)
-            .collection("clients")
-            .where("address", isEqualTo: current)
-            .get()
-            .then((value) {
-          print("Documents found: ${value.docs.length}");
-          value.docs.forEach((element) {
-            FirebaseFirestore.instance
-                .collection("allow-users")
-                .doc(user.uid)
-                .collection("clients")
-                .doc(element.id)
-                .update({"address": future})
-                //.delete()
-                .then((value) {
-              print("Success!");
-            });
-          });
-        });
-      }
-    });
-    //editClient('Tatenda');
-  }
-
-  Future<void> editClientID(String current, String future) async {
-    FirebaseAuth.instance.authStateChanges().listen((User? user) {
-      if (user != null) {
-        //print(user.uid);
-        firestoreInstance
-            .collection("allow-users")
-            .doc(user.uid)
-            .collection("clients")
-            .where("id", isEqualTo: current)
-            .get()
-            .then((value) {
-          print("Documents found: ${value.docs.length}");
-          value.docs.forEach((element) {
-            FirebaseFirestore.instance
-                .collection("allow-users")
-                .doc(user.uid)
-                .collection("clients")
-                .doc(element.id)
-                .update({"id": future})
-                //.delete()
-                .then((value) {
-              print("Success!");
-            });
-          });
-        });
-      }
-    });
-    //editClient('Tatenda');
-  }
-
-  Future<void> editClientPhone(String current, String future) async {
-    FirebaseAuth.instance.authStateChanges().listen((User? user) {
-      if (user != null) {
-        //print(user.uid);
-        firestoreInstance
-            .collection("allow-users")
-            .doc(user.uid)
-            .collection("clients")
-            .where("phone", isEqualTo: current)
-            .get()
-            .then((value) {
-          print("Documents found: ${value.docs.length}");
-          value.docs.forEach((element) {
-            FirebaseFirestore.instance
-                .collection("allow-users")
-                .doc(user.uid)
-                .collection("clients")
-                .doc(element.id)
-                .update({"phone": future})
-                //.delete()
-                .then((value) {
-              print("Success!");
-            });
-          });
-        });
-      }
-    });
-    //editClient('Tatenda');
-  }
-
-  Future<dynamic> getClients() async {
-    // CollectionReference collectionRef =
-    //     FirebaseFirestore.instance.collection("clients");
-    // // .doc(FirebaseAuth.instance.currentUser?.uid)
-    // // .collection("likedMovies");
-    // QuerySnapshot querySnapshot = await collectionRef.get();
-
+  Future<dynamic> getNotes() async {
     CollectionReference _collectionRef = FirebaseFirestore.instance
         .collection("allow-users")
         .doc(FirebaseAuth.instance.currentUser?.uid)
-        .collection("clients");
+        .collection("notes");
     QuerySnapshot querySnapshot = await _collectionRef.get();
 
-    clientNames.addAll(querySnapshot.docs.map((doc) => doc["name"]).toList());
-    IDs.addAll(querySnapshot.docs.map((doc) => doc["id"]).toList());
-    emails.addAll(querySnapshot.docs.map((doc) => doc["email"]).toList());
-    phones.addAll(querySnapshot.docs.map((doc) => doc["phone"]).toList());
-    all_details
-        .addAll(querySnapshot.docs.map((doc) => doc["details"]).toList());
-    addresses.addAll(querySnapshot.docs.map((doc) => doc["address"]).toList());
-    all_diagnoses
-        .addAll(querySnapshot.docs.map((doc) => doc["diagnosis"]).toList());
-    print(clientNames);
-    return clientNames;
+    titles.addAll(querySnapshot.docs.map((doc) => doc["title"]).toList());
+    notes.addAll(querySnapshot.docs.map((doc) => doc["note"]).toList());
+    updates_times.addAll(querySnapshot.docs.map((doc) => doc["note"]).toList());
+    print(titles);
+    return titles;
   }
 }
